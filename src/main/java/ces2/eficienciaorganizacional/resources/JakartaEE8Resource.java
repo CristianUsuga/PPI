4e7a45650a81cb4ac76bc60a7320b67f1e78e79f
usuga <usuga@LAPTOP-A124D7F0>
2022-10-19 13:36:42 -0500

    Inicio index

---
+++ src/main/java/ces2/eficienciaorganizacional/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package ces2.eficienciaorganizacional.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
